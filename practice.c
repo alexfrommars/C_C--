f55bcf51f7dcdaaed8a5c48bbb20a700442ef3da
Alexandra Scott <alexandra.scott@utexas.edu>
2020-09-15 18:09:37 -0500

    build

--- practice.c
+++ practice.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+/* print practice. printf() has many format specifiers. */
 
 int main(void){
     int i;
     double x;
 
     for(i = 1; i <10000; i *= 10)
-        printf("%d\n", i);
+        printf("2%d\n", i);
+
+    for(x = 1.234; x < 10000; x *= 10)
+        printf("%2lf\n", x);/* */
 }
